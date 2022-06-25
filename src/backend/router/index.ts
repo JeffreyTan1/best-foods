0896d8446569f0991c1c5ec32f301429534d02a8
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-06-25 19:52:07 +1000

    setup router and expose api

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,7 +1,5 @@
 import * as trpc from '@trpc/server';
-import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
-
 export const appRouter = trpc
   .router()
   .query('hello', {
@@ -20,8 +18,3 @@ export const appRouter = trpc
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
-// export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => null,
-});
