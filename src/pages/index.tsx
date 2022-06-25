6597f007a34e73740c879ca411d155bc3bb57cfe
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-06-25 20:10:14 +1000

    add trpc - working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
+import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 interface ComparisonItemProps {
   title: string;
   image_path?: string;
 }
+const ComparisonItem = ({ title }: ComparisonItemProps) => {
+  return <div className="w-1/3 text-center">{title}</div>;
+};
 
 const Home: NextPage = () => {
-  const ComparisonItem = ({ title }: ComparisonItemProps) => {
-    return <div className="w-1/3 text-center">{title}</div>;
-  };
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Jeff" }]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (data) return <div>{data.greeting}</div>;
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl">Which is better?</div>
       <div className="p-4" />
-      <div className="w-1/3 p-4 flex justify-between border-2 rounded">
+      <div className="w-1/3 p-4 flex justify-between items-center border-2 rounded">
         <ComparisonItem title="Guzman Y Gomez" />
         <ComparisonItem title="VS" />
         <ComparisonItem title="Nandos" />
