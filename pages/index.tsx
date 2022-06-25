117995f63d008552a3ca9d4a72654a806e939856
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-06-25 15:34:36 +1000

    initial page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from "next";
 
+interface ComparisonItemProps {
+  title: string;
+  image_path?: string;
+}
+
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">hello world</div>;
+  const ComparisonItem = ({ title }: ComparisonItemProps) => {
+    return <div className="w-1/3 text-center">{title}</div>;
+  };
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl">Which is better?</div>
+      <div className="p-4" />
+      <div className="w-1/3 p-4 flex justify-between border-2 rounded">
+        <ComparisonItem title="Guzman Y Gomez" />
+        <ComparisonItem title="VS" />
+        <ComparisonItem title="Nandos" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ project-blog.md
@@ -0,0 +1,7 @@
+# Instruction flow from Theo @ Ping Labs
+
+1. init.tips for nextjs setup
+2. setup tailwind via tailwind-next docs
+3. setup git 
+4. deploy on vercel for cicd, go to dashboard and setup domain (can use a subdomain of already owned domain)
+5. trpc
